8bc7c0a4b4246e52ff4abf2a9590cb85af24f2d5
joongwon323 <gold3648@kookmin.ac.kr>
2020-06-28 19:27:26 +0900

    sum & mul output

---
+++ template.py
@@ -0,0 +1,13 @@
+html = b"""
+<html>
+    <body>
+        <form action="">
+            num1 = <input type="number" name="num1"><br><br>
+            num2 = <input type="number" name="num2"><br><br>
+            <input type="submit">
+        </form>
+        sum = %(sum)d <br><br>
+        mul = %(mul)d
+    </body>
+</html>
+"""
